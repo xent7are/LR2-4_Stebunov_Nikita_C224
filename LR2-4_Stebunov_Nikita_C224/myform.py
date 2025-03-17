e642d9ed1cd1687b157b9b5b089632695c389a43
Stebunov Nikita <xent7are557@gmail.com>
2025-03-17 21:41:53 +0300

    Добавлен файл-обработчик myform.py для формы. обратной связи Добавлены: 1) Логика получения данных, которые вводит пользователь на сайте. 2) Проверка заполненности всех полей формы. 3) Проверка формата email с использованием регулярного выражения. 4) Отображение текущей даты в формате YYYY-MM-DD. 5) Сообщение пользователю теперь включает имя, email и дату обращения. 6) Возвращаются соответствующие сообщения об ошибках при неверном вводе электронной почты или незаполненных полях.

---
+++ LR2-4_Stebunov_Nikita_C224/myform.py
@@ -0,0 +1,36 @@
+from bottle import post, request, response
+import re
+from datetime import datetime
+
+@post('/home', method='post')
+def my_form():
+    
+    # Получение данных из формы
+    question = request.forms.get('QUEST', '').strip()
+    email = request.forms.get('ADRESS', '').strip()
+    username = request.forms.get('USERNAME', '').strip()
+    
+     # Проверка заполненности полей
+    if not email or not username or not question:
+        response.status = 400  # Установка статуса кода 400 для неверного запроса
+        return "All fields must be filled in. Please fill in the email, username, and question fields."
+
+    # Список разрешенных доменов
+    allowed_domains = ['gmail.com', 'mail.ru', 'inbox.ru', 'yandex.ru']
+    
+    # Паттерн для проверки адреса электронной почты
+    domain_pattern = '|'.join(re.escape(domain) for domain in allowed_domains)
+    pattern = rf'^[a-zA-Z0-9_.+-]+@({domain_pattern})$'
+    
+    # Проверка формата электронной почты
+    if not re.match(pattern, email):
+        # Установка статуса кода 400 для неверного запроса
+        response.status = 400
+        return "Incorrect email address format. Please try again!"
+
+    # Получение текущей даты в формате YYYY-MM-DD
+    access_date = datetime.now().strftime('%Y-%m-%d')
+
+     # Возвращение сообщения с именем пользователя и датой обращения
+    return f"Thanks, {username}!<br>The answer will be sent to the mail {email}.<br>Access Date: {access_date}"
+
--- LR2-4_Stebunov_Nikita_C224/views/layout.tpl
+++ LR2-4_Stebunov_Nikita_C224/views/layout.tpl
@@ -35,7 +35,8 @@
 
         <h3> Ask a Question </h3>
         <form action="/home" method="post">
-            <p><textarea rows="2" cols="50" name="QUEST" placeholder="Your question"></textarea></p> 
+            <p><textarea rows="2" cols="50" name="QUEST" placeholder="Your question"></textarea></p>
+            <p><input type="text" size="50" name="USERNAME" placeholder="Your name"></p>
             <p><input type="text" size="50" name="ADRESS" placeholder="Your email"></p>
             <p><input class="btn btn-default" type="submit" value="Send"></p>
         </form>
